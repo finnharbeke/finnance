ece1a61125f0e48b688605c9b76cb849dc0b1e11
MoriMotonari <finn.harbeke@gmail.com>
2021-12-31 13:16:59 +0100

    csrf and cookies keys to hidden file

--- config.py
+++ config.py
@@ -21,9 +21,9 @@ THREADS_PER_PAGE = 2
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
 CSRF_ENABLED     = True
 
-# Use a secure, unique and absolutely secret key for
-# signing the data. 
-CSRF_SESSION_KEY = "secret"
-
-# Secret key for signing cookies
-SECRET_KEY = "secret"
+with open("keys.txt") as f:
+    # Use a secure, unique and absolutely secret key for
+    # signing the data. 
+    CSRF_SESSION_KEY = f.readline().strip()
+    # Secret key for signing cookies
+    SECRET_KEY = f.readline().strip()
