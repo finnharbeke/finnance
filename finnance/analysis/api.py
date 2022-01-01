42e7e6cef21fa868867b8229c58e2899f4219a64
MoriMotonari <finn.harbeke@gmail.com>
2022-01-01 19:04:26 +0100

    reverse divstack order expenses

--- finnance/analysis/api.py
+++ finnance/analysis/api.py
@@ -148,7 +148,7 @@ def divstackbars(curr_id):
  
 
     # reverse expenses so that order is correct in the negative part
-    cats = category_filter(is_expense=False).order_by(Category.order.asc()).all() + (
+    cats = category_filter(is_expense=False).order_by(Category.order.desc()).all() + (
            category_filter(is_expense=True).all())
     return jsonify({
         'categories': data,
