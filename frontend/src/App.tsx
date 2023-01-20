d30a193ba88145bf26fa66eed6a6dcc162825c30
finnharbeke <code@finnharbeke.ch>
2023-01-20 13:28:31 +0100

    minor change app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import * as React from 'react';
-import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
 import { useColorScheme } from '@mantine/hooks';
+import { useEffect, useState } from 'react';
 import { RouterProvider } from "react-router-dom";
-import { FinnanceRouter } from './routes/Router';
-import theme from './theme'
 import AuthProvider from './contexts/AuthProvider';
 import ErrorModalProvider from './contexts/ErrorHandlerProvider';
+import { FinnanceRouter } from './routes/Router';
+import theme from './theme';
 
 
 function App() {
     const preferredColorScheme = useColorScheme();
-    const [colorScheme, setColorScheme] = React.useState<ColorScheme>(preferredColorScheme);
+    const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
     const toggleColorScheme = (value?: ColorScheme) =>
         setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-    theme.colorScheme = colorScheme
-
     return (
         <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-            <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+            <MantineProvider theme={{ ...theme, colorScheme }} withGlobalStyles withNormalizeCSS>
                 <ErrorModalProvider>
                     <AuthProvider>
                         <RouterProvider router={FinnanceRouter} />
