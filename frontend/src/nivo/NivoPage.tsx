2cf9b2bb58fc0defb004dfccc196db5cb8b9f3cb
finnharbeke <code@finnharbeke.ch>
2023-04-28 18:57:40 +0200

    better form on nivopage

--- frontend/src/nivo/NivoPage.tsx
+++ frontend/src/nivo/NivoPage.tsx
@@ -1,57 +1,78 @@
-import { Center, Grid, NumberInput, SimpleGrid, Stack, Title } from "@mantine/core";
+import { Alert, Center, Grid, NumberInput, SimpleGrid, Stack, Text, Title, useMantineTheme } from "@mantine/core";
+import { MonthPickerInput } from "@mantine/dates";
 import { useForm } from "@mantine/form";
-import { useViewportSize } from "@mantine/hooks";
+import { useColorScheme, useElementSize } from "@mantine/hooks";
 import { DateTime, Duration } from "luxon";
+import { TbAlertCircle } from "react-icons/tb";
 import CurrencyInput from "../components/input/CurrencyInput";
 import useIsPhone from "../hooks/useIsPhone";
 import FinnanceSunburst from "./Sunburst";
 
 interface FormValues {
     currency_id: string
-    n_months: number
+    latest: Date
+    n_rows: number
+    n_cols: number
 }
 
 export default function NivoPage() {
+    const isPhone = useIsPhone();
+    const theme = useMantineTheme();
+
     const form = useForm<FormValues>({
         initialValues: {
             currency_id: '',
-            n_months: 6
+            latest: new Date(),
+            n_cols: isPhone ? 2 : 3,
+            n_rows: isPhone ? 3 : 2
         }
     })
 
-    const { width } = useViewportSize();
-    const isPhone = useIsPhone();
+    const total = form.values.n_cols * form.values.n_rows;
 
     return <>
-        <Grid mb='md'>
-            <Grid.Col span={12} md={6}>
+        <Grid>
+            <Grid.Col span={6} md={3}>
                 <CurrencyInput hasDefault label='currency' {...form.getInputProps('currency_id')} />
             </Grid.Col>
-            <Grid.Col span={12} md={6}>
-                <NumberInput label='# months' min={1} max={48} {...form.getInputProps('n_months')}/>
+            <Grid.Col span={6} md={3}>
+                <MonthPickerInput label='until' maxDate={new Date()} {...form.getInputProps('latest')} />
+            </Grid.Col>
+            <Grid.Col span={6} md={3}>
+                <NumberInput label='# rows' min={1} max={isPhone ? 15 : 10} {...form.getInputProps('n_rows')} />
+            </Grid.Col>
+            <Grid.Col span={6} md={3}>
+                <NumberInput label='# columns' min={1} max={isPhone ? 4 : 6} {...form.getInputProps('n_cols')} />
             </Grid.Col>
         </Grid>
-        <SimpleGrid cols={1} breakpoints={[
-            { minWidth: 'xs', cols: Math.min(2, form.values.n_months) },
-            { minWidth: 'sm', cols: Math.min(3, form.values.n_months) },
-            { minWidth: 'md', cols: Math.min(4, form.values.n_months) },
-            { minWidth: 'lg', cols: Math.min(6, form.values.n_months) },
-        ]}>
+        {
+            total >= 20 &&
+            <Alert variant={theme.colorScheme === 'dark' ? 'outline' : 'light'} mt='sm'
+                icon={<TbAlertCircle />} color="yellow"
+                title='careful! the site may get slow or unresponsive with too many graphs'>
+            </Alert>
+        }
+        <SimpleGrid mt='md' cols={form.values.n_cols}>
             {
                 form.values.currency_id &&
-                Array(form.values.n_months).fill(0).map((_, i) =>
-                    <Stack key={i}>
-                        <Center>
-                            <Title order={4}>{DateTime.now().startOf('month').minus(Duration.fromObject({ months: i })).toFormat('MMMM yy')}</Title>
-                        </Center>
-                        <FinnanceSunburst size={Math.min(Math.max(isPhone ? 300 : 150, 0.8 * width / form.values.n_months), 500)} currency_id={form.values.currency_id}
-                            min_date={DateTime.now().startOf('month').minus(Duration.fromObject({ months: i }))}
-                            max_date={DateTime.now().endOf('month').minus(Duration.fromObject({ months: i }))}
-                        />
-                    </Stack>
+                Array(form.values.n_rows * form.values.n_cols).fill(0).map((_, i) =>
+                    <Cell key={i} offset={i} values={form.values}/>
                 )
             }
         </SimpleGrid>
 
     </>
+}
+
+const Cell = ({ offset, values }: { offset: number, values: FormValues }) => {
+    const { ref, width } = useElementSize();
+    return <Stack ref={ref}>
+        <Center>
+            <Title order={4}>{DateTime.fromJSDate(values.latest).startOf('month').minus(Duration.fromObject({ months: offset })).toFormat('MMM yy')}</Title>
+        </Center>
+        <FinnanceSunburst size={Math.min(width, 700)} currency_id={values.currency_id}
+            min_date={DateTime.fromJSDate(values.latest).startOf('month').minus(Duration.fromObject({ months: offset }))}
+            max_date={DateTime.fromJSDate(values.latest).endOf('month').minus(Duration.fromObject({ months: offset }))}
+        />
+    </Stack>
 }
