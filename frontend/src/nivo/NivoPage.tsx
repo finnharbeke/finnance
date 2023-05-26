606198f3a0d1a210b2edb168edf65466f3e8974b
finnharbeke <code@finnharbeke.ch>
2023-05-26 09:38:12 +0200

    tabs instead of segmented control

--- frontend/src/nivo/NivoPage.tsx
+++ frontend/src/nivo/NivoPage.tsx
@@ -1,11 +1,11 @@
 
-import { ActionIcon, Center, Grid, Group, Popover, SegmentedControl, Stack, Title } from "@mantine/core";
+import { ActionIcon, Center, Grid, Group, Popover, SegmentedControl, Stack, Tabs, Title } from "@mantine/core";
 import { MonthPicker } from "@mantine/dates";
 import { useForm } from "@mantine/form";
 import { useDisclosure, useElementSize } from "@mantine/hooks";
 import { DateTime, Duration } from "luxon";
 import { useCallback } from "react";
-import { TbCalendar, TbChartBar, TbChartDonut4, TbChevronLeft, TbChevronRight } from "react-icons/tb";
+import { TbCalendar, TbChartBar, TbChartDonut4, TbChevronLeft, TbChevronRight, TbList } from "react-icons/tb";
 import CurrencyInput from "../components/input/CurrencyInput";
 import { FinnanceBars } from "./Bars";
 import FinnanceSunburst from "./Sunburst";
@@ -13,7 +13,6 @@ import FinnanceSunburst from "./Sunburst";
 interface FormValues {
     currency_id: string
     month: Date
-    chart: 'sunburst' | 'bars'
 }
 
 export default function NivoPage() {
@@ -26,7 +25,6 @@ export default function NivoPage() {
         initialValues: {
             currency_id: '',
             month: new Date(),
-            chart: 'sunburst'
         }
     })
 
@@ -77,67 +75,61 @@ export default function NivoPage() {
                 />
             </Group>
         </Group>
-        <Grid>
-            <Grid.Col span={4} offset={4}>
-                <Title align='center'>{
-                    DateTime.fromJSDate(form.values.month)
-                        .toFormat('MMMM yy').toLowerCase()
-                }</Title>
-            </Grid.Col>
-            <Grid.Col span={4}>
-                <Group position='right'>
-                    <SegmentedControl data={[
-                        {
-                            value: 'sunburst',
-                            label: <Center><TbChartDonut4 size='1.5rem' /></Center>
-                        },
-                        {
-                            value: 'bars',
-                            label: <Center><TbChartBar size='1.5rem' /></Center>
-                        },
-                    ]} {...form.getInputProps('chart')} />
-                </Group>
-            </Grid.Col>
-        </Grid>
-        <Grid align='flex-end' gutter={'xs'}>
-            <Grid.Col ref={ref1} span={12} sm={7} order={1}>
-                <Title order={3} align='center'>expenses</Title>
-            </Grid.Col>
-            <Grid.Col ref={ref2} span={12} sm={5} orderSm={2} order={4}>
-                <Title order={3} align='center'>income</Title>
-            </Grid.Col>
-            <Grid.Col span={12} sm={7} order={2} orderSm={3}>
-                {
-                    form.values.chart === 'sunburst' ?
+        <Title align='center'>{
+            DateTime.fromJSDate(form.values.month)
+                .toFormat('MMMM yy').toLowerCase()
+        }</Title>
+        <Tabs defaultValue='sunburst'>
+            <Tabs.List position='right'>
+                <Tabs.Tab value='sunburst' icon={<TbChartDonut4 size='1.5rem' />} />
+                <Tabs.Tab value='bars' icon={<TbChartBar size='1.5rem' />} />
+                {/* <Tabs.Tab value='list' icon={<TbList size='1.5rem' />} /> */}
+            </Tabs.List>
+            <Tabs.Panel value='sunburst'>
+                <Grid align='flex-end' gutter={'xs'}>
+
+
+                    <Grid.Col ref={ref1} span={12} sm={7} order={1}>
+                        <Title order={3} align='center'>expenses</Title>
+                    </Grid.Col>
+                    <Grid.Col ref={ref2} span={12} sm={5} orderSm={2} order={4}>
+                        <Title order={3} align='center'>income</Title>
+                    </Grid.Col>
+                    <Grid.Col span={12} sm={7} order={2} orderSm={3}>
                         <FinnanceSunburst size={Math.min(width1, 500)} currency_id={form.values.currency_id}
                             min_date={DateTime.fromJSDate(form.values.month).startOf('month')}
                             max_date={DateTime.fromJSDate(form.values.month).endOf('month')}
                             is_expense={true}
                         />
-                        :
-                        <FinnanceBars currency_id={form.values.currency_id}
-                            min_date={DateTime.fromJSDate(form.values.month).startOf('month')}
-                            max_date={DateTime.fromJSDate(form.values.month).endOf('month')}
-                            is_expense={true}
-                        />
-                }
-            </Grid.Col>
-            <Grid.Col span={12} sm={5} order={5} orderSm={4}>
-                {
-                    form.values.chart === 'sunburst' ?
+                    </Grid.Col>
+                    <Grid.Col span={12} sm={5} order={5} orderSm={4}>
                         <FinnanceSunburst size={Math.min(width2, 500)} currency_id={form.values.currency_id}
                             min_date={DateTime.fromJSDate(form.values.month).startOf('month')}
                             max_date={DateTime.fromJSDate(form.values.month).endOf('month')}
                             is_expense={false}
                         />
-                        :
-                        <FinnanceBars currency_id={form.values.currency_id}
-                            min_date={DateTime.fromJSDate(form.values.month).startOf('month')}
-                            max_date={DateTime.fromJSDate(form.values.month).endOf('month')}
-                            is_expense={false}
-                        />
-                }
-            </Grid.Col>
-        </Grid>
+                    </Grid.Col>
+                </Grid>
+            </Tabs.Panel>
+            <Tabs.Panel value='bars'>
+                <Stack>
+                    <Title order={3} align='center'>expenses</Title>
+                    <FinnanceBars currency_id={form.values.currency_id}
+                        min_date={DateTime.fromJSDate(form.values.month).startOf('month')}
+                        max_date={DateTime.fromJSDate(form.values.month).endOf('month')}
+                        is_expense={true}
+                    />
+                    <Title order={3} align='center'>income</Title>
+                    <FinnanceBars currency_id={form.values.currency_id}
+                        min_date={DateTime.fromJSDate(form.values.month).startOf('month')}
+                        max_date={DateTime.fromJSDate(form.values.month).endOf('month')}
+                        is_expense={false}
+                    />
+                </Stack>
+            </Tabs.Panel>
+            {/* <Tabs.Panel value='list'>
+                hey
+            </Tabs.Panel> */}
+        </Tabs>
     </Stack>
 }
