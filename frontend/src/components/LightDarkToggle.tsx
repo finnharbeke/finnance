2c258e6c5200ef4c4e62e7abe6cb0353409884ed
finnharbeke <code@finnharbeke.ch>
2025-02-13 16:03:29 +0100

    light dark toggle style v7

--- frontend/src/components/LightDarkToggle.tsx
+++ frontend/src/components/LightDarkToggle.tsx
@@ -1,17 +1,17 @@
-import { ActionIcon, MantineTheme, useMantineColorScheme } from "@mantine/core";
+import { ActionIcon, useMantineColorScheme, useMantineTheme } from "@mantine/core";
 import { TbMoon, TbSun } from "react-icons/tb";
 
 export function LightDarkToggle() {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  
+    const theme = useMantineTheme();
     return (
         <ActionIcon
           onClick={() => toggleColorScheme()}
-          style={(theme: MantineTheme) => ({
+          style={{
             backgroundColor:
               colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
             color: colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
-          })}
+          }}
         >
           {colorScheme === 'dark' ? <TbSun size={18} /> : <TbMoon size={18} />}
         </ActionIcon>
