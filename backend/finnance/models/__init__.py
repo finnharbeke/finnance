e29e1057179357e23cd814f89214fddb16b72e52
finnharbeke <code@finnharbeke.ch>
2026-02-01 19:19:48 +0100

    models: __init__.py

---
+++ backend/finnance/models/__init__.py
@@ -0,0 +1,9 @@
+from .account import Account
+from .agent import Agent
+from .category import Category
+from .currency import Currency
+from .json import JSONModel
+from .templates import FlowTemplate, RecordTemplate, TransactionTemplate
+from .transaction import Flow, Record, Transaction
+from .transfer import AccountTransfer
+from .user import User
